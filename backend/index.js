391306bfc69ba0340392a23521ba87c67cc038f7
shefali1503 <shefalisharmaa4455@gmail.com>
2025-08-21 12:10:35 +0530

    Creating APIs for the user login

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 // Define port
 const port = 4000;
-const express = require ("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
@@ -17,118 +17,166 @@ mongoose.connect("mongodb+srv://shefalisharma:Kipl%4012345@cluster0.exmahnb.mong
 
 
 //API Creation
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Express App is running")
 })
 
 //Image Storage Engine
 
 const storage = multer.diskStorage({
-    destination : './upload/images',
-    filename : (req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage })
 
 //Creating Upload Endpoint for images
-app.use('/images',express.static('./upload/images'))
+app.use('/images', express.static('./upload/images'))
 
-app.post("/upload",upload.single('product'),(req,res)=>{
+app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
-        success : 1,
-        image_url : `http://localhost:${port}/images/${req.file.filename}`
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
     })
 })
 //upload a obect in mongo db-Schema for creating products
-const Product = mongoose.model("Product",{
-    id:{
-        type:Number,
-        required:true,
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
     },
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    image:{
-        type:String,
-        required:true,
+    image: {
+        type: String,
+        required: true,
     },
-    category:{
-        type:String,
-        required:true,
+    category: {
+        type: String,
+        required: true,
     },
-    new_price:{
-        type:Number,
-        required:true,
+    new_price: {
+        type: Number,
+        required: true,
     },
-    old_price:{
-        type:Number,
-        required:true,
+    old_price: {
+        type: Number,
+        required: true,
     },
-    date:{
-        type:Date,
-        default:Date.now,
+    date: {
+        type: Date,
+        default: Date.now,
     },
-    available:{
-        type:Boolean,
-        default:true,
+    available: {
+        type: Boolean,
+        default: true,
     },
 })
 
-app.post('/addproduct',async (req, res)=>{
+app.post('/addproduct', async (req, res) => {
     let products = await Product.find({});
     let id;
-    if(products.length>0){
+    if (products.length > 0) {
         let last_product_array = products.slice(-1);
-        let last_product =last_product_array[0];
-        id =last_product.id+1;
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
     }
-    else{
-        id=1;
+    else {
+        id = 1;
     }
     const product = new Product({
-        id:id,//auto-gernated id
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price,
+        id: id,//auto-gernated id
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
     })
     console.log(product);
     await product.save();
     console.log("Saved");
     res.json({
-        success:true,
-        name:req.body.name, 
+        success: true,
+        name: req.body.name,
     })
 })
 //Creating API for deleting products
-app.post('/removeproduct',async(req,res)=>{
-    await Product.findOneAndDelete({id:req.body.id})
+app.post('/removeproduct', async (req, res) => {
+    await Product.findOneAndDelete({ id: req.body.id })
     console.log("Removed");
     res.json({
-        success:true,
-        name:req.body.name
+        success: true,
+        name: req.body.name
     })
 
 })
 //Creating API for getting all products
-app.get('/allproducts',async(req,res)=>{
+app.get('/allproducts', async (req, res) => {
     let products = await Product.find({});
     console.log("All Products Fetched");
     res.send(products);
 })
 
 //Schema creating for User model
-
-
-app.listen(port,(error)=>{
-    if(!error){
-      console.log("Server Running on Port"+port)  
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: date,
+        default: Date.now,
     }
-    else{
-        console.log("Error :"+error)
+})
+
+//Creating Endpoint for registering the user
+app.post('/signup', async (req, res) => {
+
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+        return res.status(400).json({ success: false, errors: "existing user found with same email address" })
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port" + port)
+    }
+    else {
+        console.log("Error :" + error)
     }
 })
